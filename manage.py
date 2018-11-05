664fe5892795767336404d63b1510301d335d604
Alberto Vara <a.vara.1986@gmail.com>
2018-11-05 22:01:44 +0100

    Added pyms as lib

--- manage.py
+++ manage.py
@@ -3,22 +3,9 @@ from flask_script import Manager
 
 from project import create_app
 
-app, db = create_app()
+app = create_app()
 
 manager = Manager(app)
-
-
-@manager.command
-def create_db():
-    """Creates the db tables."""
-    db.create_all()
-
-
-@manager.command
-def drop_db():
-    """Drops the db tables."""
-    db.drop_all()
-
 
 if __name__ == '__main__':
     manager.run()
--- pyms/healthcheck/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function, absolute_import, division
-
-from flask import Blueprint
-
-healthcheck_blueprint = Blueprint('healthcheck', __name__, static_url_path='/static')
-
-from pyms.healthcheck import healthcheck
